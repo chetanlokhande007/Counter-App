5db34bf7981a3572b476ce5deeaaab3af0d98a9c
Chetan <lokhandetech1328@gmail.com>
2026-06-16 10:08:40 +0530

    Initial commit - Angular Counter App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,25 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counter-app';
+  count = 0;
+
+  handleIncrement(){
+    this.count += 1;    
+  }
+  hadleDecrement(){
+    this.count -= 1;    
+  }   
+  handleReset(){
+    this.count = 0;    
+  }
+
+
+  
 }
